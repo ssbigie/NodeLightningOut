29812c3e35dc6968038f3a532f28bf778cf2fa35
ssbigie <ssbigie@ymail.com>
2017-07-17 17:13:23 -0400

    Update app.js

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.get('/oauth/_callback', function(req, res) {
     if(!err) {
       console.log('Access Token: ' + resp.access_token);
       app.locals.oauthtoken = resp.access_token;
-      app.locals.lightningEndPointURI = "https://lightning-utilization-test-dev-ed.my.salesforce.com";
+      app.locals.lightningEndPointURI = "https://lightning-utilization-test-dev-ed.lightning.force.com";
       res.redirect('/home');
     } else {
       console.log('Error: ' + err.message);
